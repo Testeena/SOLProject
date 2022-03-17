6e8a00061bd02303649468da45f275eccf78e55b
Francesco <francescocamaccioli@gmail.com>
2022-03-17 23:29:36 +0100

    Concurrent queue implementation (tested) for client's requests

---
+++ code/includes/requestqueue.h
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <pthread.h>
+#include <signal.h>
+#include <sys/un.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+
+typedef struct reqnode{
+	int fd;
+	void* next;
+} ReqNode;
+
+typedef struct requestqueue{
+
+	// size values
+	int maxsize;
+	int currentsize;
+
+	// First Request reference
+	ReqNode* head;
+	// Last Request reference
+	ReqNode* tail;
+
+	// Concurrence Variables
+	pthread_mutex_t queuemtx;
+	pthread_cond_t empty;
+	pthread_cond_t full;
+
+} RequestQueue;
+
+RequestQueue* newRequestQueue(int maxsize);
+int freeRequestQueue(RequestQueue* queue);
+int putRequest(RequestQueue* queue, int fd);
+int getRequest(RequestQueue* queue);
---
+++ code/src/requestqueue.c
@@ -0,0 +1,164 @@
+#include "../includes/requestqueue.h"
+#include "../includes/utils.h"
+
+RequestQueue* newRequestQueue(int maxsize){
+
+	if(maxsize <= 0){
+		errno = EINVAL;
+		return NULL;
+	}
+
+	RequestQueue* new;
+	if((new = malloc(sizeof(RequestQueue))) == NULL){
+		return NULL;
+	}
+
+	PERRNOTZERO(pthread_mutex_init(&(new->queuemtx), NULL));
+	PERRNOTZERO(pthread_cond_init(&(new->empty), NULL));
+	PERRNOTZERO(pthread_cond_init(&(new->full), NULL));
+
+	new->head = new->tail = NULL;
+	new->maxsize = maxsize;
+
+	return new;
+}
+
+int freeRequestQueue(RequestQueue* queue){
+
+	while(queue->currentsize != 0){
+		ReqNode* temp = queue->head;
+		queue->head = (ReqNode*)queue->head->next;
+		queue->currentsize--;
+		free(temp);
+	}
+
+	PERRNOTZERO(pthread_cond_destroy(&queue->empty));
+	PERRNOTZERO(pthread_cond_destroy(&queue->full));
+	PERRNOTZERO(pthread_mutex_destroy(&queue->queuemtx));
+	free(queue);
+	return 0;
+}
+
+
+int putRequest(RequestQueue* queue, int fd){
+
+	if(queue == NULL){
+		errno = EINVAL;
+		return -1;
+	}
+
+	ReqNode* toput;
+	if((toput = (ReqNode*)malloc(sizeof(ReqNode))) == NULL){
+		return -1;
+	}
+
+	toput->fd = fd;
+	toput->next = NULL;
+
+	PERRNOTZERO(pthread_mutex_lock(&(queue->queuemtx)));
+
+	while(queue->currentsize == queue->maxsize){
+		PERRNOTZERO(pthread_cond_wait(&(queue->full), &(queue->queuemtx)));
+	}
+
+	if(queue->head == NULL || queue->tail == NULL){
+		queue->head = queue->tail = (ReqNode*)toput;
+	}
+	else{
+		queue->tail->next = (ReqNode*)toput;
+		queue->tail = (ReqNode*)toput;
+	}
+
+	queue->currentsize++;
+
+	if(queue->currentsize == 1){
+		PERRNOTZERO(pthread_cond_broadcast(&(queue->empty)));
+	}
+
+	PERRNOTZERO(pthread_mutex_unlock(&(queue->queuemtx)));
+	return 0;
+}
+
+int getRequest(RequestQueue* queue){
+
+	if(queue == NULL){
+		errno = EINVAL;
+		return -1;
+	}
+
+	PERRNOTZERO(pthread_mutex_lock(&(queue->queuemtx)));
+
+	while(queue->currentsize == 0){
+		PERRNOTZERO(pthread_cond_wait((&queue->empty), &(queue->queuemtx)));
+	}
+
+	ReqNode* temp = queue->head;
+	queue->head = (ReqNode*)queue->head->next;
+	queue->currentsize--;
+
+	if(queue->head == NULL){
+		queue->tail = NULL;
+	}
+
+	if(queue->currentsize == (queue->maxsize)--){
+		PERRNOTZERO(pthread_cond_broadcast(&(queue->full)));
+	}
+
+	PERRNOTZERO(pthread_mutex_unlock(&(queue->queuemtx)));
+
+	return temp->fd;
+}
+
+void printqueue(RequestQueue* queue){
+
+	ReqNode* temp = queue->head;
+
+	while(temp != NULL){
+		printf("%d->", temp->fd);
+		temp = temp->next;
+	}
+
+	puts("NULL");
+
+}
+
+int main(int argc, char const *argv[]){
+	
+	RequestQueue* queue = newRequestQueue(5);
+
+	for (int i = 1; i < 6; i++){
+		printf("putRequest responsecode: %d\n", putRequest(queue, i));
+	}
+	printqueue(queue);
+	int result = 0;
+	for (int i = 1; i < 6; i++){
+		result = getRequest(queue);
+		printf("getRequest result: %d\n", result);
+		printqueue(queue);
+	}
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
